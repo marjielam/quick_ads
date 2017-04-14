ae02d205bf42b670e7b9b28bd14868bbf7fbc7ec
Marjie Lam <marjielam@gmail.com>
2017-04-14 19:46:43 -0400

    cleaned up variables

--- myjs.js
+++ myjs.js
@@ -1,8 +1,6 @@
-const adContainer = document.querySelector('.ad-top-bar-container');
-
 document.addEventListener('animationstart', function(event) {
   if (event.animationName == 'nodeInserted') {
-    const myVideo = document.querySelector('.video-player');
-    myVideo.playbackRate = 5;
+    const video = document.querySelector('.video-player');
+    video.playbackRate = 5;
   }
 }, true);
